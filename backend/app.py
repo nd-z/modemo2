4b2e18a52fde41da7c9776c7e753a90637c2562d
Maria Sam <maria.sam@gmail.com>
2018-04-10 18:23:41 -0400

    skeleton for server

--- backend/app.py
+++ backend/app.py
@@ -8,26 +8,11 @@ from modules.article_crawler import ArticleCrawler
 app = Flask(__name__)
 crawler = ArticleCrawler()
 analyzer = BiasAnalyzer()
-# From the flask sqlalchemy configurations page on tracking modifications:
-#   If set to True, Flask-SQLAlchemy will track modifications of objects and
-#   emit signals. The default is None, which enables tracking but issues a
-#   warning that it will be disabled by default in the future. This requires
-#   extra memory and should be disabled if not needed.
 
-@app.route('/urlf', methods=['GET'])
+@app.route('/get_dict', methods=['GET'])
 def get_bias_for_article():
-    print request.args['url']
-    print('here');
-    return request.args['url']
-
-@app.route('/content', methods=['POST'])
-def hello():
-    """
-    Constructs a setting object from the POST request body and adds the new
-    setting to the airdrop setting table. Then returns the set setting.
-
-    Returns:
-        flask.wrappers.Response: Response object containing a json
-            representation of the current setting.
-    """
-    print('post')
+    url = request.args['url']
+    paragraphs = crawler.url_content(url)
+    nyt_data = paragraphs
+    totalbias, total_dict = analyzer.get_article_bias(nyt_data)
+    return jsonify(dictionary=total_dict, total_bias=totalbias)
