ebdce0c25088fa7df1611bbf4ec488c7a1a620db
Eric Feng <evf23@cornell.edu>
2018-04-10 18:42:03 -0400

    continuing progress on frontend

--- modemo-app/src/index.js
+++ modemo-app/src/index.js
@@ -1,19 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import logo from './logo.png';
 import './index.css';
+
+const url = "http://localhost:5001"
 
 class Root extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {pageValue: 'Home'};
+    this.state = {
+      pageValue: 'Home',
+      data: null,
+      error: false
+    };
+    this.changePage = this.changePage.bind(this);
+    this.updateData = this.updateData.bind(this);
+  }
+
+  changePage(page, errored) {
+    this.setState({pageValue: page});
+    this.setState({error: errored})
+  }
+
+  updateData(data) {
+    this.setState({data: data});
   }
 
   render() {
     let visiblePage = null;
     switch(this.state.pageValue) {
       case 'Home':
-        visiblePage = <LandingPage />;
+        visiblePage = <LandingPage
+                        errored={this.state.error}
+                        changePage={this.changePage}
+                        updateData={this.updateData}/>;
+        break;
+      case 'Loading':
+        visiblePage = <LoadingPage />;
+        break;
+      case 'Analysis':
+        visiblePage = <AnalysisPage data={this.state.data}/>;
         break;
       default:
         visiblePage = null;
@@ -39,11 +66,44 @@ class Header extends React.Component {
 }
 
 class LandingPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    console.log(this.props)
+    return (
+      <div className='pageContent'>
+        {this.props.errored ? <div className="alert alert-danger">There was an error in processing the article.</div> : null}
+        <h2 className='title'>Analyze a URL</h2>
+        <UrlInput changePage={this.props.changePage} updateData={this.props.updateData}/>
+        <p>
+          modemo can analyze the political bias of any news article for you.
+          Just input the article url of your choice and let Modemo show you the results.
+        </p>
+      </div>);
+  }
+}
+
+const LoadingPage = () => (
+      <div className='analysisContent'>
+        <p>
+          Analyzing...
+        </p>
+        <div className="loader"></div>
+      </div>
+);
+
+class AnalysisPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className='pageContent'>
         <h2 className='title'>Analyze a URL</h2>
-        <UrlInput />
+        <UrlInput changePage={this.props.changePage}/>
         <p>
           modemo can analyze the political bias of any news article for you.
           Just input the article url of your choice and let Modemo show you the results.
@@ -65,18 +125,28 @@ class UrlInput extends React.Component {
     this.setState({url: event.target.value});
   }
 
-  handleSubmit(event) {
-    alert('A url was submitted: ' + this.state.url);
+  handleSubmit() {
+    this.props.changePage("Loading")
+    // axios.post(url + "/analyze", {url: this.state.url})
+    // .then(response => {
+    //   this.props.changePage("Analysis")
+    //   this.props.updateData(response.data)
+    // })
+    // .catch(error => {
+    //   this.props.changePage("Home", true)
+    // })
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit} className='url-input-row'>
-        <label>
-          <input type='text' value={this.state.url} onChange={this.handleChange} placeholder='Input a URL'/>
-        </label>
-        <input type='submit' value='Submit' className='submit-button' />
-      </form>
+      <div>
+        <form className='url-input-row'>
+          <label>
+            <input type='text' value={this.state.url} onChange={this.handleChange} placeholder='Input a URL'/>
+          </label>
+        </form>
+        <button onClick={this.handleSubmit}> Analyze URL </button>
+      </div>
     );
   }
 }
